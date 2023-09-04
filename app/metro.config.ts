8d20ad899c6c52006eabda1557c428aecd64c045
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-04 20:05:09 +0200

    feat(app): Example with router

---
+++ app/metro.config.ts
@@ -0,0 +1,9 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+import { getDefaultConfig } from 'expo/metro-config';
+
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
+
+module.exports = config;
