5cb1604a32633b1b577ba926b89ce27b5c7773b3
Croos3r <dorian.moy@epitech.eu>
2023-09-04 14:51:47 +0200

    feat(backend-setup): finally setup database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'app',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
