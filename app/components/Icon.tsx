8feee0559af2a7104f029df3bfe23b8e09a335a7
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-04 23:56:01 +0200

    feat(app): Widgets bar

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,22 +1,30 @@
 import { ComponentProps } from "react";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
-export type IconProps = (
-	| {
-			name: ComponentProps<typeof FontAwesome>["name"];
-			source: "FontAwesome";
-	  }
-	| { name: ComponentProps<typeof AntDesign>["name"]; source: "AntDesign" }
-) & {
-	color: string;
-} & ComponentProps<typeof FontAwesome | typeof AntDesign>;
+type FontAwesomeIconProps = {
+	name: ComponentProps<typeof FontAwesome>["name"];
+	source: "FontAwesome";
+} & ComponentProps<typeof FontAwesome>;
+
+type AntDesignIconProps = {
+	name: ComponentProps<typeof AntDesign>["name"];
+	source: "AntDesign";
+} & ComponentProps<typeof AntDesign>;
+
+type MaterialIconsIconProps = {
+	name: ComponentProps<typeof MaterialIcons>["name"];
+	source: "MaterialIcons";
+} & ComponentProps<typeof MaterialIcons>;
+
+export type IconProps = FontAwesomeIconProps | AntDesignIconProps | MaterialIconsIconProps;
 
 const Icon = ({ name, source, color, ...props }: IconProps) => {
 	if (source === "FontAwesome") {
 		return <FontAwesome name={name} {...props} />;
 	} else if (source === "AntDesign") {
 		return <AntDesign name={name} {...props} />;
+	} else if (source === "MaterialIcons") {
+		return <MaterialIcons name={name} {...props} />;
 	}
 };
 
