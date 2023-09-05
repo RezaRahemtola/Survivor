2122437a585228c9d3ec9f91e936b503548e462e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-05 23:29:42 +0200

    feat(app): Basic weather widget

--- app/app/(tabs)/home.tsx
+++ app/app/(tabs)/home.tsx
@@ -1,14 +1,13 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import React from "react";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import CurrentWeatherForecast from "@/components/weather/CurrentWeatherForecast";
+import mockedWeather from "@/mocks/weather.json";
 
 export default function HomeScreen() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Home</Text>
-			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-			<EditScreenInfo path="app/(tabs)/home.tsx" />
+			<CurrentWeatherForecast currentWeather={mockedWeather} />
 		</View>
 	);
 }
@@ -18,14 +17,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: "center",
 		justifyContent: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
+		padding: 20,
 	},
 });
