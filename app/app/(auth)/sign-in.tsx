6affe38cfa82f02aa676412674f47ca205abbf91
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-05 10:45:37 +0200

    feat(app): real login page

--- app/app/(auth)/sign-in.tsx
+++ app/app/(auth)/sign-in.tsx
@@ -1,12 +1,45 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, TextInput, View } from "react-native";
 import { useAuthContext } from "../../context/auth";
+import { useState } from "react";
 
 export default function SignIn() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 	const { signIn } = useAuthContext();
 
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text onPress={() => signIn()}>Sign In</Text>
+		<View style={styles.container}>
+			<TextInput
+				value={username}
+				onChangeText={(value) => setUsername(value)}
+				placeholder={"Username"}
+				style={styles.input}
+			/>
+			<TextInput
+				value={password}
+				onChangeText={(value) => setPassword(value)}
+				placeholder={"Password"}
+				secureTextEntry={true}
+				style={styles.input}
+			/>
+
+			<Button title={"Login"} onPress={() => signIn()} />
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	input: {
+		width: 200,
+		height: 44,
+		padding: 10,
+		borderWidth: 1,
+		borderColor: "black",
+		marginBottom: 10,
+	},
+});
