48a1a426e9f374ae8ee2483dea4a532450caecf6
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-06 00:32:35 +0200

    fix(app): weather layout simplified

--- app/layouts/DailyWeatherForecast.tsx
+++ app/layouts/DailyWeatherForecast.tsx
@@ -1,42 +1,23 @@
-// @ts-ignore
-import styled from "styled-components/native";
+import { StyleSheet } from "react-native";
 
 import DailyWeather from "@/components/weather/DailyWeatherForecast";
+import { Text } from "@/components/Themed";
 
-const DailyWeatherForecast = ({ weather }: any) => {
-	return (
-		<>
-			{weather.daily ? (
-				weather.daily.map((day: any, index: number) => {
-					return <DailyWeather key={day.dt} day={day} index={index} />;
-				})
-			) : (
-				<NoWeather>No Weather to show</NoWeather>
-			)}
-		</>
-		// <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={{ flex: 1 }}>
-		// 	<FutureForecastContainer>
-		// 		{weather.daily ? (
-		// 			weather.daily.map((day: any, index: number) => {
-		// 				return <DailyWeather key={day.dt} day={day} index={index} />;
-		// 			})
-		// 		) : (
-		// 			<NoWeather>No Weather to show</NoWeather>
-		// 		)}
-		// 	</FutureForecastContainer>
-		// </ScrollView>
-	);
-};
+const DailyWeatherForecast = ({ weather }: any) => (
+	<>
+		{weather.daily ? (
+			weather.daily.map((day: any) => {
+				return <DailyWeather key={day.dt} day={day} />;
+			})
+		) : (
+			<Text style={styles.noWeather}>No Weather to show</Text>
+		)}
+	</>
+);
 
-const FutureForecastContainer = styled.View`
-	//display: flex;
-	//align-items: center;
-	//justify-content: center;
-`;
-
-const NoWeather = styled.Text`
-	text-align: center;
-	color: white;
-`;
-
+const styles = StyleSheet.create({
+	noWeather: {
+		textAlign: "center",
+	},
+});
 export default DailyWeatherForecast;
