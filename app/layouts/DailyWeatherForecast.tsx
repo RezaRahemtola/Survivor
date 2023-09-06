aed11988e45a6dc0ab771b14dd431547a59dae31
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-06 12:11:07 +0200

    fix(app): Weather loading state and useless checks

--- app/layouts/DailyWeatherForecast.tsx
+++ app/layouts/DailyWeatherForecast.tsx
@@ -3,18 +3,25 @@ import { ScrollView, StyleSheet } from "react-native";
 import DailyWeather from "@/components/weather/DailyWeatherForecast";
 import { Text } from "@/components/Themed";
 
-const DailyWeatherForecast = ({ weather }: any) => (
-	<ScrollView>
-		{weather.daily ? (
-			weather.daily.map((day: any) => {
-				return <DailyWeather key={day.dt} day={day} />;
-			})
-		) : (
-			<Text style={styles.noWeather}>No Weather to show</Text>
-		)}
-	</ScrollView>
-);
+const DailyWeatherForecast = ({ weather, errorMsg }: { weather: any; errorMsg: string | undefined }) => {
+	if (!weather && !errorMsg) {
+		return <Text>Daily weather forecast loading...</Text>;
+	} else if (!weather && errorMsg) {
+		return <Text>{errorMsg}</Text>;
+	}
 
+	return (
+		<ScrollView>
+			{weather.daily ? (
+				weather.daily.map((day: any) => {
+					return <DailyWeather key={day.dt} day={day} />;
+				})
+			) : (
+				<Text style={styles.noWeather}>No Weather to show</Text>
+			)}
+		</ScrollView>
+	);
+};
 const styles = StyleSheet.create({
 	noWeather: {
 		textAlign: "center",
