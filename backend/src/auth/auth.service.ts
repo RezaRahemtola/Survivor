e5a1751a3d6a3750d2a2581e15f34265a1c67132
Croos3r <dorian.moy@epitech.eu>
2023-09-07 00:19:57 +0200

    refactor(login-endpoint): removed useless picture and user profile of the response
    Took 18 minutes

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,31 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { LoginResultDto, MasuraoLoginResultDto } from './dto/login-result.dto';
+import { MasuraoLoginResultDto } from './dto/login-result.dto';
 import MasuraoCredentialsDto from './dto/credentials.dto';
 import { runHttpRequestWithData } from '../http';
-import { EmployeesService } from '../employees/employees.service';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly employeesService: EmployeesService,
-  ) {}
+  constructor(private readonly httpService: HttpService) {}
 
-  async logIn(credentials: MasuraoCredentialsDto): Promise<LoginResultDto> {
+  async logIn(
+    credentials: MasuraoCredentialsDto,
+  ): Promise<MasuraoLoginResultDto> {
     const { access_token } = await runHttpRequestWithData<
       MasuraoLoginResultDto,
       MasuraoCredentialsDto
     >(this.httpService.axiosRef, 'post', '/employees/login', credentials);
-    const me = await this.employeesService.getSelfEmployeeLong(access_token);
-
     return {
       access_token,
-      picture: await this.employeesService.getEmployeePicture(
-        me.id,
-        access_token,
-      ),
-      ...me,
     };
   }
 }
