c5faeee0eefc31c767b104f9ffc557d8c2bc33ac
Croos3r <dorian.moy@epitech.eu>
2023-09-06 15:55:21 +0200

    refactor(login-endpoint): refactor to use new employees' service
    Took 17 minutes

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import CredentialsDto from './dto/credentials.dto'
+import MasuraoCredentialsDto from './dto/credentials.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +8,7 @@ export class AuthController {
   }
 
   @Post('login')
-  logIn(@Body() credentials: CredentialsDto) {
+  logIn(@Body() credentials: MasuraoCredentialsDto) {
     return this.authService.logIn(credentials)
   }
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common'
-import { HttpModule } from '@nestjs/axios'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
+import { EmployeesModule } from '../employees/employees.module'
 
 @Module({
-  imports: [
-    HttpModule.register({
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'X-Group-Authorization': '5roNHlfWme2GWpfbKyKeL0_7Ld0u5g7i',
-      },
-      baseURL: 'https://masurao.fr/api',
-    }),
-  ],
+  imports: [EmployeesModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
