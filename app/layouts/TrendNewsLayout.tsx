38355c2a1e3fb693dd8bd60910250b9aa1347d57
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-07 18:15:08 +0200

    feat(app/widgets): News widget with backend usage

--- app/layouts/TrendNewsLayout.tsx
+++ app/layouts/TrendNewsLayout.tsx
@@ -1,30 +1,29 @@
 import { StyleSheet } from "react-native";
-import axios from "axios";
 import { useAtom } from "jotai";
 import { Text } from "@/components/Themed";
 
 import { newsAtom } from "@/stores/widgets";
 import NewsSlider from "@/components/news/NewsSlider";
 import { ActivityIndicator } from "react-native-paper";
+import axios from "@/config/axios";
+import { TrendingNewsResult } from "@/types/news";
+import { useEffect } from "react";
 
 const TrendNewsTitle = () => <Text style={styles.title}>Trending News</Text>;
 
 const TrendNewsLayout = () => {
 	const [news, setNews] = useAtom(newsAtom);
 
-	const fetchNews = () => {
-		axios
-			.get("https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=920deb9f754348c0bec4871fef36d971")
-			.then((response) => {
+	useEffect(() => {
+		(async () => {
+			if (!news) {
+				const response = await axios.get<TrendingNewsResult>("/external/news?country=us");
 				setNews(response.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+			}
+		})();
+	}, []);
 
 	if (!news) {
-		fetchNews();
 		return (
 			<>
 				<TrendNewsTitle />
---
+++ app/types/news.ts
@@ -0,0 +1,19 @@
+export type TrendingNewsArticle = {
+	source: {
+		id: string;
+		name: string;
+	};
+	author: string;
+	title: string;
+	description: string;
+	url: string;
+	urlToImage: string;
+	publishedAt: string;
+	content: string;
+};
+
+export type TrendingNewsResult = {
+	status: "ok" | "error";
+	totalResults: number;
+	articles: TrendingNewsArticle[];
+};
