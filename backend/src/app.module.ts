7b357c85591a7cac8d86fd4081f3d5073a0c09f8
Croos3r <dorian.moy@epitech.eu>
2023-09-08 19:15:04 +0200

    feat(user-settings): made user settings api endpoints (get, update, reset), removed user widgets feature
    Took 2 hours 48 minutes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,8 +5,8 @@ import { EmployeesModule } from './employees/employees.module';
 import { GlobalModule } from './global.module';
 import { ExternalApisModule } from './external-apis/external-apis.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { WidgetsModule } from './widgets/widgets.module';
-import UserWidgets from './widgets/model/user-widgets.entity';
+import { UserSettingsModule } from './user-settings/user-settings.module';
+import UserSettings from './user-settings/entities/user-settings.entity';
 
 @Module({
   imports: [
@@ -25,7 +25,7 @@ import UserWidgets from './widgets/model/user-widgets.entity';
         username: config.getOrThrow<string>('POSTGRES_USERNAME'),
         password: config.getOrThrow<string>('POSTGRES_PASSWORD'),
         database: config.getOrThrow<string>('POSTGRES_DATABASE'),
-        entities: [UserWidgets],
+        entities: [UserSettings],
         synchronize: true,
       }),
     }),
@@ -33,7 +33,7 @@ import UserWidgets from './widgets/model/user-widgets.entity';
     AuthModule,
     EmployeesModule,
     ExternalApisModule,
-    WidgetsModule,
+    UserSettingsModule,
   ],
   controllers: [],
   providers: [],
