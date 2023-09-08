ed721f1ae4dd25ae37865f9bcaf8cb1a7ac9e130
Croos3r <dorian.moy@epitech.eu>
2023-09-08 23:46:27 +0200

    feat(api-dev-logging): made the app logs requests and response in dev environment
    Took 45 minutes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
   await app.listen(3000);
 }
 bootstrap();
