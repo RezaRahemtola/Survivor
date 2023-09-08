4ce3e84a00989a6c8b04c23400db3b1839e45845
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-08 03:35:33 +0200

    feat(app/widgets): Selection added

--- app/app/(tabs)/widgets.tsx
+++ app/app/(tabs)/widgets.tsx
@@ -1,14 +1,91 @@
 import { StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import { useEffect, useState } from "react";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import { View } from "@/components/Themed";
+import { WidgetType } from "@/types/widgets";
+import SelectDropdown from "react-native-select-dropdown";
+import { getAccessToken } from "@/cache/accessToken";
+import axios from "@/config/axios";
+import { useAtom } from "jotai/index";
+import { widgetsAtom } from "@/stores/widgets";
+
+const WidgetSelector = ({
+	widget,
+	onValueChange,
+	onRemove,
+}: {
+	widget: WidgetType | undefined;
+	onValueChange: (value: WidgetType) => void;
+	onRemove: () => void;
+}) => {
+	return (
+		<View style={{ flexDirection: "row" }}>
+			<SelectDropdown
+				data={["trendingNews", "currentWeather", "weatherWeekForecast"]}
+				defaultValue={widget}
+				onSelect={(item) => onValueChange(item)}
+			/>
+			<Button icon="delete" onPress={onRemove}></Button>
+		</View>
+	);
+};
 
 export default function WidgetsScreen() {
+	const [, setWidgetsAtom] = useAtom(widgetsAtom);
+	const [widgets, setWidgets] = useState<(WidgetType | undefined)[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const accessToken = await getAccessToken();
+				const response = await axios.get<WidgetType[]>(`/widgets`, {
+					headers: { Authorization: `Bearer ${accessToken}` },
+				});
+				setWidgets(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, []);
+
+	const saveWidgets = async () => {
+		const accessToken = await getAccessToken();
+		const newWidgets = widgets.filter((widget) => widget !== undefined) as WidgetType[];
+		await axios.patch(
+			"/widgets",
+			{
+				newWidgetNames: newWidgets,
+			},
+			{ headers: { Authorization: `Bearer ${accessToken}` } },
+		);
+		setWidgetsAtom(newWidgets);
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Widgets</Text>
-			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-			<EditScreenInfo path="app/(tabs)/widgets.tsx" />
+			{widgets.map((widget, index) => (
+				<WidgetSelector
+					key={index}
+					widget={widget}
+					onValueChange={(value: WidgetType) => {
+						const dupWidgets = [...widgets];
+						dupWidgets.splice(index, 1, value);
+						setWidgets(dupWidgets);
+					}}
+					onRemove={() => {
+						const dupWidgets = [...widgets];
+						dupWidgets.splice(index, 1);
+						setWidgets(dupWidgets);
+					}}
+				/>
+			))}
+			<Button style={styles.addWidget} icon="plus" mode="contained" onPress={() => setWidgets([...widgets, undefined])}>
+				Add a widget
+			</Button>
+			<Button style={styles.save} buttonColor={"grey"} mode="contained" onPress={saveWidgets}>
+				Save
+			</Button>
 		</View>
 	);
 }
@@ -27,5 +104,11 @@ const styles = StyleSheet.create({
 		marginVertical: 30,
 		height: 1,
 		width: "80%",
+	},
+	addWidget: {
+		marginTop: 15,
+	},
+	save: {
+		marginTop: 5,
 	},
 });
