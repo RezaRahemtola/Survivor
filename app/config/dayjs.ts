6d21d64a89572cf9f7083e07152f8c92280677a3
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-09 13:06:31 +0200

    fix(app): Weather weekday locale with i18n

--- app/config/dayjs.ts
+++ app/config/dayjs.ts
@@ -1,6 +1,14 @@
 import dayjs from "dayjs";
 import weekday from "dayjs/plugin/weekday";
+import en from "dayjs/locale/en";
+import fr from "dayjs/locale/fr";
+import es from "dayjs/locale/es";
 
 dayjs.extend(weekday);
 
+export const locales = {
+	en,
+	fr,
+	es,
+};
 export default dayjs;
