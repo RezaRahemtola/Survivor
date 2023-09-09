9e7b924fb570ce88164fb87d33455d3ecd14483c
Croos3r <dorian.moy@epitech.eu>
2023-09-09 16:53:17 +0200

    feat(swagger-documentation): setup swagger documentation page
    Took 13 minutes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageJson from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+  const config = new DocumentBuilder()
+    .setTitle('TrombiDay API')
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
