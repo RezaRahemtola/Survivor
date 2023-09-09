de928467d554f541c194e7d5626fd6c6fcb47b49
Croos3r <dorian.moy@epitech.eu>
2023-09-09 20:21:19 +0200

    feat(swagger-documentation): fully documented user settings section
    Took 11 minutes

--- backend/src/user-settings/dto/user-settings-update.dto.ts
+++ backend/src/user-settings/dto/user-settings-update.dto.ts
@@ -7,17 +7,31 @@ import {
   Widget,
   WIDGETS,
 } from '../entities/user-settings.entity';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UserSettingsUpdateDto {
+  @ApiPropertyOptional({
+    description: 'Widgets to be displayed on the dashboard',
+    enum: WIDGETS,
+    isArray: true,
+  })
   @IsOptional()
   @IsArray()
   @IsEnum(WIDGETS, { each: true })
   widgets?: Widget[];
 
+  @ApiPropertyOptional({
+    description: 'Language of the interface',
+    enum: LANGUAGES,
+  })
   @IsOptional()
   @IsEnum(LANGUAGES)
   language?: Language;
 
+  @ApiPropertyOptional({
+    description: 'Interface scheme',
+    enum: INTERFACE_SCHEMES,
+  })
   @IsOptional()
   @IsEnum(INTERFACE_SCHEMES)
   interfaceScheme?: InterfaceScheme;
--- backend/src/user-settings/entities/user-settings.entity.ts
+++ backend/src/user-settings/entities/user-settings.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 export const WIDGETS = [
   'currentWeather',
@@ -19,15 +20,31 @@ export const DEFAULT_USER_SETTINGS: Omit<UserSettings, 'email'> = {
 
 @Entity()
 export default class UserSettings {
+  @ApiProperty({
+    description: 'Email of the user',
+  })
   @PrimaryColumn()
   email!: string;
 
+  @ApiProperty({
+    description: "Widgets displayed on the user's application",
+    enum: WIDGETS,
+    isArray: true,
+  })
   @Column({ type: 'enum', enum: WIDGETS, array: true, default: [] })
   widgets!: Widget[];
 
+  @ApiProperty({
+    description: "Language of the user's interface",
+    enum: LANGUAGES,
+  })
   @Column({ type: 'enum', enum: LANGUAGES, default: 'en' })
   language!: Language;
 
+  @ApiProperty({
+    description: "Interface scheme of the user's interface",
+    enum: INTERFACE_SCHEMES,
+  })
   @Column({ type: 'enum', enum: INTERFACE_SCHEMES, default: 'auto' })
   interfaceScheme!: InterfaceScheme;
 }
