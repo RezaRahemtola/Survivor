c9b61f6f5b5dba3cabddab64b7d32858ee78339e
Croos3r <dorian.moy@epitech.eu>
2023-09-09 17:16:19 +0200

    feat(swagger-documentation): fully typed auth section
    Took 23 minutes

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,30 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import MasuraoCredentialsDto from './dto/credentials.dto';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiProduces,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Authentification')
+@ApiProduces('application/json')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiBody({
+    description: 'Masurao credentials of the user',
+    type: MasuraoCredentialsDto,
+  })
+  @ApiForbiddenResponse({
+    description: 'Invalid credentials',
+  })
+  @ApiCreatedResponse({
+    description: 'User successfully authorized in',
+  })
   @Post('login')
   logIn(@Body() credentials: MasuraoCredentialsDto) {
     return this.authService.logIn(credentials);
