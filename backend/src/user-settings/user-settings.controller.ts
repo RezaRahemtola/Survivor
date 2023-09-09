db047be8c66fd5e54c312e4beb8a4269c36b2a45
Croos3r <dorian.moy@epitech.eu>
2023-09-09 20:21:19 +0200

    feat(swagger-documentation): fully documented user settings section
    Took 11 minutes

--- backend/src/user-settings/user-settings.controller.ts
+++ backend/src/user-settings/user-settings.controller.ts
@@ -3,17 +3,47 @@ import { UserSettingsService } from './user-settings.service';
 import { APIRequest } from '../token-aware-cache.interceptor';
 import JwtAuthGuard from '../auth/jwt-auth.guard';
 import { UserSettingsUpdateDto } from './dto/user-settings-update.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiForbiddenResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import UserSettings from './entities/user-settings.entity';
 
+@ApiTags('User settings')
+@ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 @Controller('user-settings')
 export class UserSettingsController {
   constructor(private readonly userSettingsService: UserSettingsService) {}
 
+  @ApiResponse({
+    status: '2XX',
+    description: 'Current user settings',
+    type: UserSettings,
+  })
+  @ApiForbiddenResponse({
+    description: 'Invalid access token',
+  })
   @Get()
   async getSelfUserSettings(@Req() { user: { email } }: APIRequest) {
     return this.userSettingsService.getSelfUserSettings(email);
   }
 
+  @ApiBody({
+    description: 'New user settings',
+    type: UserSettingsUpdateDto,
+  })
+  @ApiResponse({
+    status: '2XX',
+    description: 'Updated user settings',
+    type: UserSettings,
+  })
+  @ApiForbiddenResponse({
+    description: 'Invalid access token',
+  })
   @Patch()
   async updateSelfUserSettings(
     @Req() { user: { email } }: APIRequest,
@@ -22,6 +52,14 @@ export class UserSettingsController {
     return this.userSettingsService.updateSelfUserSettings(email, newSettings);
   }
 
+  @ApiResponse({
+    status: '2XX',
+    description: 'Resetted user settings',
+    type: UserSettings,
+  })
+  @ApiForbiddenResponse({
+    description: 'Invalid access token',
+  })
   @Patch('reset')
   async resetUserSettings(@Req() { user: { email } }: APIRequest) {
     return this.userSettingsService.resetUserSettings(email);
