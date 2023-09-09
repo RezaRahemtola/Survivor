4abdc392193e59c89de65411f2c0fb48c725d6b4
Croos3r <dorian.moy@epitech.eu>
2023-09-09 17:16:19 +0200

    feat(swagger-documentation): fully typed auth section
    Took 23 minutes

--- backend/src/auth/dto/credentials.dto.ts
+++ backend/src/auth/dto/credentials.dto.ts
@@ -1,10 +1,18 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class MasuraoCredentialsDto {
+  @ApiProperty({
+    description: 'Email of the user',
+    example: 'name.surname@masurao.ext',
+  })
   @IsEmail()
   @IsNotEmpty()
   email!: string;
 
+  @ApiProperty({
+    description: 'Password of the user',
+  })
   @IsString()
   @IsNotEmpty()
   password!: string;
--- backend/src/auth/dto/login-result.dto.ts
+++ backend/src/auth/dto/login-result.dto.ts
@@ -1,6 +1,10 @@
 import { IsJWT } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class MasuraoLoginResultDto {
+  @ApiProperty({
+    description: 'JWT access token',
+  })
   @IsJWT()
   access_token!: string;
 }
