292af8e2122b4dcbb3de04acc3ef885d0bfa0938
Croos3r <dorian.moy@epitech.eu>
2023-09-12 17:47:22 +0200

    feat(chat): made cache mappings for id and email clients
    Took 39 minutes

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,11 +1,14 @@
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import {
+  Inject,
   UseFilters,
   UseGuards,
   UsePipes,
@@ -15,6 +18,9 @@ import { ChatAuthGuard } from './chat-auth.guard';
 import MessageSendDto from './dto/message-send.dto';
 import { JwtPayload } from '../auth/jwt.strategy';
 import { BadRequestTransformationFilter } from './bad-request-transformation.filter';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { AuthService } from 'src/auth/auth.service';
 
 @UsePipes(new ValidationPipe())
 @UseFilters(BadRequestTransformationFilter)
@@ -24,9 +30,16 @@ import { BadRequestTransformationFilter } from './bad-request-transformation.fil
     origin: '*',
   },
 })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server!: Socket;
+
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+    @Inject(AuthService)
+    private readonly authService: AuthService,
+  ) {}
 
   @SubscribeMessage('global-message')
   handleMessage(
@@ -36,5 +49,27 @@ export class ChatGateway {
     { email }: JwtPayload,
   ) {
     this.server.emit('global-message', { sender: email, message });
+  }
+
+  async handleConnection(client: Socket) {
+    try {
+      const token = client.handshake.headers.authorization.split(' ')[1];
+      const { email } = this.authService.verifyToken(token);
+      const clients =
+        (await this.cacheManager.get<Set<string>>(`e2is/${email}`)) ??
+        new Set<string>();
+      clients.add(client.id);
+      await this.cacheManager.set(`e2is/${email}`, clients);
+      await this.cacheManager.set(`i2e/${client.id}`, email);
+    } catch (e) {
+      client.emit('events', 'Invalid token');
+      client.disconnect(true);
+    }
+  }
+
+  async handleDisconnect(client: Socket) {
+    const email = await this.cacheManager.get(`i2e/${client.id}`);
+    await this.cacheManager.del(`e2is/${email}`);
+    await this.cacheManager.del(`i2e/${client.id}`);
   }
 }
