5aef369c53832181c15bc0303c0b753198387d90
Croos3r <dorian.moy@epitech.eu>
2023-09-13 16:37:41 +0200

    feat(discord-webhook-service): added service to send an embed with several possible styles
    Took 45 minutes

--- backend/src/external-apis/external-apis.service.ts
+++ backend/src/external-apis/external-apis.service.ts
@@ -10,6 +10,27 @@ import {
   WeatherDataDto,
 } from './dto/location.dto';
 
+export const DISCORD_WEBHOOK_EMBED_TYPES = {
+  info: {
+    color: 0x0000ff,
+    authorName: '❕ Information',
+  },
+  success: {
+    color: 0x00ff00,
+    authorName: '✅ Success',
+  },
+  warn: {
+    color: 0xffff00,
+    authorName: '⚠️ Warning',
+  },
+  error: {
+    color: 0xff0000,
+    authorName: '❌ Error',
+  },
+} as const;
+export type DiscordWebhookEmbedStatus =
+  keyof typeof DISCORD_WEBHOOK_EMBED_TYPES;
+
 @Injectable()
 export class ExternalApisService {
   constructor(
@@ -103,6 +124,48 @@ export class ExternalApisService {
       webhookUrl,
       {
         content: message,
+        username,
+        avatar_url,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  }
+
+  async sendDiscordWebhookEmbed(
+    message: string,
+    title?: string,
+    status: DiscordWebhookEmbedStatus = 'info',
+  ) {
+    const webhookUrl = this.configService.getOrThrow<string>(
+      'DISCORD_WEBHOOK_URL',
+    );
+    const username = this.configService.get<string>(
+      'DISCORD_WEBHOOK_USERNAME',
+      'TrombiDay',
+    );
+    const avatar_url = this.configService.get<string>(
+      'DISCORD_WEBHOOK_AVATAR_URL',
+    );
+    const embedType = DISCORD_WEBHOOK_EMBED_TYPES[status];
+    await runHttpRequestWithData<never>(
+      this.httpService.axiosRef,
+      'post',
+      webhookUrl,
+      {
+        embeds: [
+          {
+            title,
+            description: message,
+            author: {
+              name: embedType.authorName,
+            },
+            color: embedType.color,
+          },
+        ],
         username,
         avatar_url,
       },
