f3f70396a0e9e0d0a851a18724b2a75f5e427753
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-14 17:34:56 +0200

    feat: Global messages

--- app/components/MessageSender.tsx
+++ app/components/MessageSender.tsx
@@ -1,102 +1,71 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { StyleSheet, View, TextInput, TouchableWithoutFeedback, useColorScheme } from "react-native";
-import { Socket, io } from 'socket.io-client';
+import { StyleSheet, TextInput, TouchableWithoutFeedback, useColorScheme, View } from "react-native";
+import { Socket } from "socket.io-client";
 import { useAtom } from "jotai";
 import { MessageReceiveAtom } from "@/stores/chat";
-import { getAccessToken } from "@/cache/accessToken";
-import { Icon, SocialIcon } from "react-native-elements";
+import Icon from "@/components/Icon";
 
-export const MessageSender = () => {
+export const MessageSender = ({ socket }: { socket: Socket }) => {
+	const [message, setMessage] = useState(String);
+	const [, setMessageReceived] = useAtom(MessageReceiveAtom);
 
-    const [message, setMessage] = useState(String);
-    const [, setMessageReceived] = useAtom(MessageReceiveAtom)
-    const [socket, setSocket] = useState<Socket | undefined>(undefined);
+	const sendMessage = (message: string) => {
+		socket.emit("global-message", { message: `${message} (${socket.id})` });
+		setMessageReceived((oldList) => [...oldList, { message: message, email: "Me" }]);
+		setMessage("");
+	};
 
-    useEffect(() => {
-        const createSocket = async () => {
-            const token = await getAccessToken();
-            const socket = io(`${process.env.EXPO_PUBLIC_API_URL}`, {transportOptions: {
-                polling: {
-                    extraHeaders: {
-                        Authorization: `Bearer ${token}`,
-                    }
-                }
-            }});
-            setSocket(socket);
-            socket.on('connect', function() {
-                console.log('Connectedsender');
-            });
+	const colorScheme = useColorScheme();
+	const { t } = useTranslation();
 
-            socket.on('events', function(data) {
-                console.log('event', data);
-            });
-
-            socket.on('exception', function(data) {
-                console.error('exception', data);
-            });
-
-            socket.on('disconnect', function() {
-                console.warn('Disconnected');
-            });
-        };
-        createSocket();
-    }, []);
-
-    const sendMessage = (message: string) => {
-        socket?.emit('global-message', {message: `${message} (${socket.id})`});
-        setMessageReceived(oldList => [...oldList, {message: message, email: "Me"}]);
-        setMessage("");
-    };
-
-    const colorScheme = useColorScheme();
-    const { t } = useTranslation();
-
-    return (
-        <View style={{ flexDirection: "row" }}>
-            <TextInput
-                style={ colorScheme === "dark" ? styles.textInputWhite : styles.textInputBlack }
-                placeholder={t("chat.sendMessage")}
-                placeholderTextColor={colorScheme === "dark" ? "white" : "black"}
-                onChangeText={setMessage}
-                value={message}
-            />
-            <TouchableWithoutFeedback onPress={() => {if (message) sendMessage(message)}}>
-                <Icon name="send" source="MaterialIcons" size={25} style={styles.icon} color={ colorScheme === "dark" ? "#FFFFFF" : "#000000" } />
-            </TouchableWithoutFeedback>
-        </View>
-    )
+	return (
+		<View style={{ flexDirection: "row" }}>
+			<TextInput
+				style={colorScheme === "dark" ? styles.textInputWhite : styles.textInputBlack}
+				placeholder={t("chat.sendMessage")}
+				placeholderTextColor={colorScheme === "dark" ? "white" : "black"}
+				onChangeText={setMessage}
+				value={message}
+			/>
+			<TouchableWithoutFeedback
+				onPress={() => {
+					if (message) sendMessage(message);
+				}}
+			>
+				<Icon name="send" source="FontAwesome" size={25} style={styles.icon} />
+			</TouchableWithoutFeedback>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    textInputWhite: {
-        margin: 10,
-        padding: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
-        borderRadius: 30,
-        backgroundColor: "transparent",
-        borderColor: '#FFFFFF',
-        borderWidth: 1,
-        color: "#FFFFFF",
-        flex: 8,
-    },
-    textInputBlack: {
-        margin: 10,
-        padding: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
-        borderRadius: 30,
-        backgroundColor: "transparent",
-        borderColor: '#000000',
-        borderWidth: 1,
-        color: "#000000",
-        flex: 8,
-    },
-    icon: {
-        alignSelf: 'flex-end',
-        marginRight: 15,
-        marginTop: 25,
-        flex: 2
-    }
+	textInputWhite: {
+		margin: 10,
+		padding: 10,
+		paddingLeft: 20,
+		paddingRight: 20,
+		borderRadius: 30,
+		backgroundColor: "transparent",
+		borderColor: "#FFFFFF",
+		borderWidth: 1,
+		color: "#FFFFFF",
+		flex: 8,
+	},
+	textInputBlack: {
+		margin: 10,
+		padding: 10,
+		paddingLeft: 20,
+		paddingRight: 20,
+		borderRadius: 30,
+		backgroundColor: "transparent",
+		borderColor: "#000000",
+		borderWidth: 1,
+		color: "#000000",
+		flex: 8,
+	},
+	icon: {
+		alignSelf: "center",
+		marginRight: 15,
+	},
 });
--- app/components/home/ChatWidget.tsx
+++ app/components/home/ChatWidget.tsx
@@ -1,35 +1,56 @@
-import { Text, View } from "@/components/Themed";
+import { Text, useThemeColor, View } from "@/components/Themed";
 import { useEffect, useState } from "react";
 import { StyleSheet, TouchableWithoutFeedback, useColorScheme } from "react-native";
-import { getAccessToken } from "@/cache/accessToken";
-import Icon, { IconProps } from "@/components/Icon";
+import Icon from "@/components/Icon";
 import { MessageSender } from "@/components/MessageSender";
 import { router } from "expo-router";
-import { io } from 'socket.io-client';
+import { io, Socket } from "socket.io-client";
 import { MessageReceiveAtom } from "@/stores/chat";
 import { MessageReceiveData } from "@/types/chat";
 import { useTranslation } from "react-i18next";
 import { useAtom } from "jotai";
+import { getAccessToken } from "@/cache/accessToken";
+import { Card } from "react-native-elements";
 
-const latestMessage = (message: MessageReceiveData[]) => {
+const LatestMessage = ({ message }: { message: MessageReceiveData[] }) => {
 	const colorScheme = useColorScheme();
 	const { t } = useTranslation();
 
 	if (message.length === 0) {
-		return (
-			<Text style={styles.NoMessage} >No Message</Text>
-		);
+		return <Text style={styles.NoMessage}>No Message</Text>;
 	}
 	if (message.length === 1) {
 		return (
 			<View>
-				<View style={[styles.HeaderMessageReceive, message.at(-1)?.email === "Me" ? { flexDirection: "row", justifyContent: "flex-end" } : { flexDirection: "row", justifyContent: "flex-start" }]}>
-					<Text style={[styles.TextMessage]}> {message.at(-1)?.email === "Me" ? t("chat.me") : message.at(-1)?.email}</Text>
+				<View
+					style={[
+						styles.HeaderMessageReceive,
+						message.at(-1)?.email === "Me"
+							? {
+									flexDirection: "row",
+									justifyContent: "flex-end",
+							  }
+							: { flexDirection: "row", justifyContent: "flex-start" },
+					]}
+				>
+					<Text style={[styles.TextMessage]}>
+						{" "}
+						{message.at(-1)?.email === "Me" ? t("chat.me") : message.at(-1)?.email}
+					</Text>
 				</View>
-				<View style={message.at(-1)?.email === "Me" ? { flexDirection: "row", justifyContent: "flex-end" } : { flexDirection: "row", justifyContent: "flex-start" }}>
-				<View style={[styles.messageReceived, {backgroundColor: colorScheme === "dark" ? "#FFFFFF" : "#000000"}]}>
-					<Text style={{color: colorScheme === "dark" ? "#000000" : "#FFFFFF"}}> {message.at(-1)?.message} </Text>
-				</View>
+				<View
+					style={
+						message.at(-1)?.email === "Me"
+							? {
+									flexDirection: "row",
+									justifyContent: "flex-end",
+							  }
+							: { flexDirection: "row", justifyContent: "flex-start" }
+					}
+				>
+					<View style={[styles.messageReceived, { backgroundColor: colorScheme === "dark" ? "#FFFFFF" : "#000000" }]}>
+						<Text style={{ color: colorScheme === "dark" ? "#000000" : "#FFFFFF" }}> {message.at(-1)?.message} </Text>
+					</View>
 				</View>
 			</View>
 		);
@@ -37,93 +58,145 @@ const latestMessage = (message: MessageReceiveData[]) => {
 	if (message.length > 1) {
 		return (
 			<View>
-				<View style={[styles.HeaderMessageReceive, message.at(-2)?.email === "Me" ? { flexDirection: "row", justifyContent: "flex-end" } : { flexDirection: "row", justifyContent: "flex-start" }]}>
-					<Text style={[styles.TextMessage]}> {message.at(-1)?.email === "Me" ? t("chat.me") : message.at(-1)?.email}</Text>
+				<View
+					style={[
+						styles.HeaderMessageReceive,
+						message.at(-2)?.email === "Me"
+							? {
+									flexDirection: "row",
+									justifyContent: "flex-end",
+							  }
+							: { flexDirection: "row", justifyContent: "flex-start" },
+					]}
+				>
+					<Text style={[styles.TextMessage]}>
+						{" "}
+						{message.at(-1)?.email === "Me" ? t("chat.me") : message.at(-1)?.email}
+					</Text>
 				</View>
-				<View style={message.at(-2)?.email === "Me" ? { flexDirection: "row", justifyContent: "flex-end" } : { flexDirection: "row", justifyContent: "flex-start" }}>
-				<View style={[styles.messageReceived, {backgroundColor: colorScheme === "dark" ? "#FFFFFF" : "#000000"}]}>
-					<Text style={{color: colorScheme === "dark" ? "#000000" : "#FFFFFF"}}> {message.at(-2)?.message} </Text>
+				<View
+					style={
+						message.at(-2)?.email === "Me"
+							? {
+									flexDirection: "row",
+									justifyContent: "flex-end",
+							  }
+							: { flexDirection: "row", justifyContent: "flex-start" }
+					}
+				>
+					<View style={[styles.messageReceived, { backgroundColor: colorScheme === "dark" ? "#FFFFFF" : "#000000" }]}>
+						<Text style={{ color: colorScheme === "dark" ? "#000000" : "#FFFFFF" }}> {message.at(-2)?.message} </Text>
+					</View>
 				</View>
+				<View
+					style={[
+						styles.HeaderMessageReceive,
+						message.at(-1)?.email === "Me"
+							? {
+									flexDirection: "row",
+									justifyContent: "flex-end",
+							  }
+							: { flexDirection: "row", justifyContent: "flex-start" },
+					]}
+				>
+					<Text style={[styles.TextMessage]}>
+						{" "}
+						{message.at(-1)?.email === "Me" ? t("chat.me") : message.at(-1)?.email}
+					</Text>
 				</View>
-				<View style={[styles.HeaderMessageReceive, message.at(-1)?.email === "Me" ? { flexDirection: "row", justifyContent: "flex-end" } : { flexDirection: "row", justifyContent: "flex-start" }]}>
-					<Text style={[styles.TextMessage]}> {message.at(-1)?.email === "Me" ? t("chat.me") : message.at(-1)?.email}</Text>
-				</View>
-				<View style={message.at(-1)?.email === "Me" ? { flexDirection: "row", justifyContent: "flex-end" } : { flexDirection: "row", justifyContent: "flex-start" }}>
-				<View style={[styles.messageReceived, {backgroundColor: colorScheme === "dark" ? "#FFFFFF" : "#000000"}]}>
-					<Text style={{color: colorScheme === "dark" ? "#000000" : "#FFFFFF"}}> {message.at(-1)?.message} </Text>
-				</View>
+				<View
+					style={
+						message.at(-1)?.email === "Me"
+							? {
+									flexDirection: "row",
+									justifyContent: "flex-end",
+							  }
+							: { flexDirection: "row", justifyContent: "flex-start" }
+					}
+				>
+					<View style={[styles.messageReceived, { backgroundColor: colorScheme === "dark" ? "#FFFFFF" : "#000000" }]}>
+						<Text style={{ color: colorScheme === "dark" ? "#000000" : "#FFFFFF" }}> {message.at(-1)?.message} </Text>
+					</View>
 				</View>
 			</View>
 		);
 	}
 };
 
-let counter = 0;
+class TrombiSocket {
+	private static instance: Socket;
 
-const ChatWidget = () => {
-    const [, setMessageReceived] = useAtom(MessageReceiveAtom)
-	const colorScheme = useColorScheme();
-	const { t } = useTranslation();
-	const [messages,] = useAtom(MessageReceiveAtom);
-
-    useEffect(() => {
-		const createSocket = async () => {
-            if (counter > 0) return;
-            counter += 1;
-			console.log("Here");
+	public static async getInstance(oldMessages: MessageReceiveData[], setMessages: any) {
+		if (!TrombiSocket.instance) {
 			const token = await getAccessToken();
-			const socket = io(`${process.env.EXPO_PUBLIC_API_URL}`, {transportOptions: {
-				polling: {
-					extraHeaders: {
-						Authorization: `Bearer ${token}`
-					}
-				}
-			}});
-
-			socket.on('connect', function() {
-				console.log('ConnectedReceiver');
+			TrombiSocket.instance = io(`${process.env.EXPO_PUBLIC_API_URL}`, {
+				transportOptions: {
+					polling: {
+						extraHeaders: {
+							Authorization: `Bearer ${token}`,
+						},
+					},
+				},
+			});
+			TrombiSocket.instance.on("connect", function () {
+				console.log("ConnectedReceiver");
 			});
 
-			socket.on('global-message', function({sender, message}) {
+			TrombiSocket.instance.on("global-message", function ({ sender, message }) {
 				console.log(`${sender} said: ${message}`);
-				setMessageReceived(oldList => [...oldList, {message: message, email: sender}]);
+				setMessages([...oldMessages, { message: message, email: sender }]);
 			});
 
-			socket.on('events', function(data) {
-				console.log('Revent', data);
+			TrombiSocket.instance.on("events", function (data) {
+				console.log("Revent", data);
 			});
 
-			socket.on('exception', function(data) {
-				console.error('Rexception', data);
+			TrombiSocket.instance.on("exception", function (data) {
+				console.error("Rexception", data);
 			});
 
-			socket.on('disconnect', function() {
-				console.warn('RDisconnected');
+			TrombiSocket.instance.on("disconnect", function () {
+				console.warn("RDisconnected");
 			});
-        };
-        createSocket();
-    }, []);
+		}
 
-    return (
-        <View style={styles.container}>
-            <View style={[styles.tchatPreview, {borderWidth: 2, borderColor: colorScheme === "dark" ? "#FFFFFF" : "#000000"}]}>
-                <View style={[styles.Header]}>
-                    <Text style={[styles.Title]}> {t("chat.title")} </Text>
-                    <TouchableWithoutFeedback onPress={() => router.push("/home/modal")}>
-                        <Icon name="resize-full-screen" source="Entypo" size={25} style={{ alignSelf: 'center'}} />
-                    </TouchableWithoutFeedback>
-                </View>
-                {latestMessage(messages)}
-                <MessageSender/>
-            </View>
-        </View>
-    );
+		return TrombiSocket.instance;
+	}
 }
 
+const ChatWidget = () => {
+	const [messages, setMessageReceived] = useAtom(MessageReceiveAtom);
+	const [socket, setSocket] = useState<Socket | undefined>(undefined);
+	const { t } = useTranslation();
+	const backgroundColor = useThemeColor({}, "background");
+
+	useEffect(() => {
+		(async () => {
+			setSocket(await TrombiSocket.getInstance(messages, setMessageReceived));
+		})();
+	}, []);
+
+	return (
+		<>
+			{socket ? (
+				<Card containerStyle={{ backgroundColor }}>
+					<View style={styles.Header}>
+						<Text style={styles.Title}>{t("chat.title")} </Text>
+						<TouchableWithoutFeedback onPress={() => router.push("/home/modal")}>
+							<Icon name="resize-full-screen" source="Entypo" size={25} style={{ alignSelf: "center" }} />
+						</TouchableWithoutFeedback>
+					</View>
+					<LatestMessage message={messages} />
+					<MessageSender socket={socket} />
+				</Card>
+			) : (
+				<></>
+			)}
+		</>
+	);
+};
+
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
 	tchatPreview: {
 		marginTop: 20,
 		borderRadius: 20,
@@ -135,35 +208,35 @@ const styles = StyleSheet.create({
 	},
 	messageReceived: {
 		maxWidth: "50%",
-        paddingHorizontal: 15,
-        paddingTop: 10,
-        paddingBottom: 15,
-        borderRadius: 25,
-        margin: 10,
-        marginTop: 3,
+		paddingHorizontal: 15,
+		paddingTop: 10,
+		paddingBottom: 15,
+		borderRadius: 25,
+		margin: 10,
+		marginTop: 3,
 	},
 	HeaderMessageReceive: {
-		marginLeft: 10
+		marginLeft: 10,
 	},
 	TextMessage: {
 		marginRight: 20,
 		marginLeft: 20,
-		fontSize: 12
+		fontSize: 12,
 	},
 	Header: {
-		flexDirection: 'row',
-  		justifyContent: 'center',
-		backgroundColor: 'transparent'
+		flexDirection: "row",
+		justifyContent: "center",
+		backgroundColor: "transparent",
 	},
 	MessageSender: {
-		flexDirection: 'row',
+		flexDirection: "row",
 		marginHorizontal: 10,
 		paddingTop: 20,
 	},
 	NoMessage: {
 		fontSize: 40,
-		alignSelf: 'center',
-	}
+		alignSelf: "center",
+	},
 });
 
 export default ChatWidget;
