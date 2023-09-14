a0769ec01a515fc05b5b14102475aedea42e6840
Croos3r <dorian.moy@epitech.eu>
2023-09-14 17:26:03 +0200

    feat(mocking-jay-mode): made a switch on env variable to enable the use of the mock values
    Took 1 hour 11 minutes

--- backend/src/employees/employees.module.ts
+++ backend/src/employees/employees.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
 import { UserSettingsModule } from '../user-settings/user-settings.module';
+import { EmployeesOfflineService } from './employees-offline.service';
 
 @Module({
   imports: [UserSettingsModule],
   controllers: [EmployeesController],
-  providers: [EmployeesService],
-  exports: [EmployeesService],
+  providers: [EmployeesService, EmployeesOfflineService],
+  exports: [EmployeesService, EmployeesOfflineService],
 })
 export class EmployeesModule {}
