ecd28fa874a1bebab14aecf1b06b8e08f67c663f
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-15 01:24:11 +0200

    fix: Typing

--- app/config/socket.ts
+++ app/config/socket.ts
@@ -5,12 +5,14 @@ import { MessageReceiveData } from "@/types/chat";
 import axios from "@/config/axios";
 import { FullUser } from "@/types/user";
 
+type SetMessagesFunc = (prev: MessageReceiveData[]) => MessageReceiveData[];
+
 export class ChatSocket {
 	private static instance: Socket;
 	private static email: string;
 	private static setupStarted: boolean = false;
 
-	public static async getInstance(setMessages: any) {
+	public static async getInstance(setMessages: (func: SetMessagesFunc) => void) {
 		if (!ChatSocket.instance && !ChatSocket.setupStarted) {
 			ChatSocket.setupStarted = true;
 			const accessToken = await getAccessToken();
